9a0c77cec7fe62fb31ebe05a0da23e3cba8c69e4
batchunaveen <batchunaveen6@gmail.com>
2017-10-08 09:26:48 -0400

    Initial Commit

---
+++ DrinkingWater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrinkingWater
+//
+//  Created by Naveen Batchu on 10/8/17.
+//  Copyright © 2017 NaveenInnovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
