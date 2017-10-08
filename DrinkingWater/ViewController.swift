eae0f3616252f382b95f1438c246d07c0691ad81
batchunaveen <batchunaveen6@gmail.com>
2017-10-08 13:32:39 -0400

    Measuring the  glasses of Water Intake

--- DrinkingWater/ViewController.swift
+++ DrinkingWater/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterView : CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pushButtonPressed(_ button : PushButton){
+       
+            if button.isAddButton {
+                counterView.counter += 1
+            } else {
+                if counterView.counter > 0 {
+                    counterView.counter -=  1
+                }
+            }
+        counterLabel.text = String(counterView.counter)
     }
 
 
